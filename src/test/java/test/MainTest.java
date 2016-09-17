1fd38251b8b832fbd5e0835ab2bb11f09e768102
ZaiweiXiong <Zaiwei.xiong@gmail.com>
2016-09-17 21:29:09 +0800

    updated MainTest.java

---
+++ src/test/java/test/MainTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import com.algorithm;
+import org.testng.annotations.Test;
+
+/**
+ * Created by David on 2016/9/17.
+ */
+public class MainTest {
+
+    @Test
+    public void testMethod(){
+
+        //algorithm.lengthOfLongestSubstring("11");
+        System.out.println("testMethod");
+
+    }
+}
