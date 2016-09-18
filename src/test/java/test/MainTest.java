079544820a61f70f3d22a9e8c3b743f78cb16ae8
ZaiweiXiong <Zaiwei.xiong@gmail.com>
2016-09-18 22:14:16 +0800

    updated MainTest.java

--- src/test/java/test/MainTest.java
+++ src/test/java/test/MainTest.java
@@ -2,7 +2,10 @@ package test;
 
 
 import com.Solution;
+import com.TestStack;
 import org.testng.annotations.Test;
+
+import java.util.Stack;
 
 
 /**
@@ -24,6 +27,16 @@ public class MainTest {
     public void testMethodtwo(){
 
         System.out.println("This is test method two!");
+        Stack stack = new Stack();
+        // populating stack
+        stack.push("Java");
+        stack.push("Source");
+        stack.push("code");
+        System.out.println("Removed object is: "+stack.pop());
+        System.out.println("Elements after remove: "+stack);
+        System.out.println("Top object is: "+stack.peek());
+        System.out.println("Searching 'code' in stack: "+stack.search("Source"));
+        System.out.println("Is stack empty: "+stack.empty());
     }
 
 
