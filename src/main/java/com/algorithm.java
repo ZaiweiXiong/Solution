1c4eecbef4d5615a1cc116213ccd903a25f4b4df
unknown <zaiwei.xiong@gmail.com>
2019-06-18 12:23:14 +0800

    updatedfiles

--- src/main/java/com/algorithm.java
+++ src/main/java/com/algorithm.java
@@ -163,5 +163,35 @@ public class algorithm {
 		}
 		  return -1;
 	}
+	public int removeDupNumber(int [] a, int n) {
+
+           if (n==0||n==1) {
+               return n;
+           }
+            
+            int j = 0;
+            int[] temp = new int[n];
+
+            for (int i = 0; i < n - 1;i++ )
+            {
+                if (a[i]!=a[i+1]) {
+
+                    temp[j++]=a[i];
+                
+                }
+            }
+
+            temp[j++] = a[n - 1];
+
+            for (int i = 0; i < j;i++ )
+            {
+                a[i] = temp[i];
+            }
+
+
+            return j;
+
+        }
+
 	
 }
